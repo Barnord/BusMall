c1fee5822d4a0d8699e81da06412c4defe95ffbb
Ben Arno <bdarno92@gmail.com>
2021-05-19 14:18:54 -0700

    lab13 done

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const data = document.getElementById('data')
+const buttonDiv = document.getElementById('buttonDiv')
 const dataChart = document.getElementById('chart')
 const results = document.getElementById('results')
 const productContainer = document.getElementById('productContainer')
@@ -22,11 +23,11 @@ let currentProductOne = null;
 let currentProductTwo = null;
 let currentProductThree = null;
 
-function Product(name, imgPath) {
+function Product(name, imgPath, shown, votes) {
   this.name = name;
   this.imgPath = imgPath;
-  this.shown = 0;
-  this.votes = 0;
+  this.shown = shown;
+  this.votes = votes;
 
   Product.allProducts.push(this);
 }
@@ -87,7 +88,7 @@ function makeButton() {
   const button = document.createElement('button');
   button.addEventListener('click', displayResults)
   button.textContent = 'View Data';
-  data.appendChild(button);
+  buttonDiv.appendChild(button);
 }
 
 function handleClick(e) {
@@ -111,12 +112,13 @@ function handleClick(e) {
     }
   } else {
       productContainer.removeEventListener('click', handleClick);
-      // renderResults();
       makeButton();
   }
 }
 
 function displayResults() {
+  buttonDiv.innerHTML = ''
+  updateStorage();
   renderResults();
   getDataSets();
   renderViewChart();
@@ -163,28 +165,47 @@ function renderViewChart() {
   });
 }
 
+function updateStorage() {
+  const stringifiedProducts = JSON.stringify(Product.allProducts);
+  localStorage.setItem('products', stringifiedProducts);
+}
+
+function callStorage() {
+  let returnedProducts = localStorage.getItem('products');
+  if (returnedProducts) {
+    let parsedProducts = JSON.parse(returnedProducts);
+    Product.allProducts = [];
+    for (let product of parsedProducts) {
+      new Product(product.name, product.imgPath, product.shown, product.votes);
+    }
+  } else {
+    console.log('Nothing matching in storage.')
+  }
+}
+
 productContainer.addEventListener('click', handleClick)
 
-new Product('R2D2 Suitcase', './assets/bag.jpg' );
-new Product('Banana Cutter', './assets/banana.jpg');
-new Product('Swipe & Wipe', './assets/bathroom.jpg');
-new Product('Toeless Rainboots', './assets/boots.jpg');
-new Product('All-in-one Breakfast Maker', './assets/breakfast.jpg');
-new Product('Meatball Bubble Gum', './assets/bubblegum.jpg');
-new Product('Uncomfortable Chair', './assets/chair.jpg');
-new Product('Cthulhu Action Figure', './assets/cthulhu.jpg');
-new Product('Dog Duck Beak!', './assets/dog-duck.jpg');
-new Product('Dragon Meat', './assets/dragon.jpg');
-new Product('Pen Untensils', './assets/pen.jpg');
-new Product('Pet Sweep', './assets/pet-sweep.jpg');
-new Product('Pizza Scissors', './assets/scissors.jpg');
-new Product('Sleeping Shark', './assets/shark.jpg');
-new Product('Baby Sweep', './assets/sweep.png');
-new Product('Tauntaun Sleeping Bag', './assets/tauntaun.jpg');
-new Product('Unicorn Meat', './assets/unicorn.jpg');
-new Product('Self-watering Can', './assets/water-can.jpg');
-new Product('No Spill Wine Glass', './assets/wine-glass.jpg');
+new Product('R2D2 Suitcase', './assets/bag.jpg', 0, 0);
+new Product('Banana Cutter', './assets/banana.jpg', 0, 0);
+new Product('Swipe & Wipe', './assets/bathroom.jpg', 0, 0);
+new Product('Toeless Rainboots', './assets/boots.jpg', 0, 0);
+new Product('All-in-one Breakfast Maker', './assets/breakfast.jpg', 0, 0);
+new Product('Meatball Bubble Gum', './assets/bubblegum.jpg', 0, 0);
+new Product('Uncomfortable Chair', './assets/chair.jpg', 0, 0);
+new Product('Cthulhu Action Figure', './assets/cthulhu.jpg', 0, 0);
+new Product('Dog Duck Beak!', './assets/dog-duck.jpg', 0, 0);
+new Product('Dragon Meat', './assets/dragon.jpg', 0, 0);
+new Product('Pen Untensils', './assets/pen.jpg', 0, 0);
+new Product('Pet Sweep', './assets/pet-sweep.jpg', 0, 0);
+new Product('Pizza Scissors', './assets/scissors.jpg', 0, 0);
+new Product('Sleeping Shark', './assets/shark.jpg', 0, 0);
+new Product('Baby Sweep', './assets/sweep.png', 0, 0);
+new Product('Tauntaun Sleeping Bag', './assets/tauntaun.jpg', 0, 0);
+new Product('Unicorn Meat', './assets/unicorn.jpg', 0, 0);
+new Product('Self-watering Can', './assets/water-can.jpg', 0, 0);
+new Product('No Spill Wine Glass', './assets/wine-glass.jpg', 0, 0);
 
+callStorage();
 pickProducts();
 console.log(currentProductOne);
 console.log(currentProductTwo);
